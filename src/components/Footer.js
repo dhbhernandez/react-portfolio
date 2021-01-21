59928678b367094e592a2f5f446625b455882052
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2021-01-20 20:54:45 -0600

    updating components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="position-fixed fixed-bottom text-center p-1 bg-dark text-white">
+        Copyright 2020
+    </footer>
+    );
+}
+
+export default Footer;
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -8,45 +8,38 @@ function NavTabs() {
   const location = useLocation();
 
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/about"
-          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/blog"
-          className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
-        >
-          Blog
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact"
-          className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact/learn"
-          className={location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"}
-        >
-          Learn
-        </Link>
-      </li>
-    </ul>
+    <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+      {/* <a class="navbar-brand" href="index.html">Diego Hernandez</a> */}
+      <Link to="/about" className={"navbar-brand"}>
+              Diego Hernandez
+                </Link>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <Link to="/about" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+              About
+                </Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/portfolio" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+              Portfolio
+                </Link>
+
+          </li>
+          <li class="nav-item">
+            <Link to="/contact" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+              Contact
+                </Link>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
   );
 }
 
