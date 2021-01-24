9aaa947f1fcbbc72209a3db40fdddd769c69c2ce
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2021-01-23 22:16:25 -0600

    deleted containers not needed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
@@ -12,10 +11,8 @@ import './App.css'
 function App() {
   return (
     <Router>
-      <div className="app" style={{backgroundImage:`url(${img})`}}>
-        
+      <div className="app" style={{ backgroundImage: `url(${img})` }}>
         <NavTabs />
-        <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
